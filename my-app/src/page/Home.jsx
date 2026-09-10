fddd59e4dc43494b4007417e703c01c1da1c1539
สิรวิชญ์ บุญญาธิ <boonyathi_s@su.ac.th>
2026-09-10 06:21:48 +0000

    ready to deploy

--- my-app/src/page/Home.jsx
+++ my-app/src/page/Home.jsx
@@ -1,8 +1,22 @@
+import {movies} from '../data';
+import MovieCard from '../components/MovieCard';
+import MobieGallery from '../components/MovieGallery';
+
 function Home() {
   return (
     <div className="p-8 text-center">
       <h1 className="text-3xl font-bold text-slate-800">🏠 หน้าแรก</h1>
       <p className="mt-2 text-slate-500">ยินดีต้อนรับสู่ MovieHub</p>
+      <h2 className="mt-8 text-2xl font-bold text-slate-800">หนังแนะนำ</h2>
+      <div className="mx-auto mt-8 max-w-5xl">
+        {movies.slice(0,3).map((movie) => (
+          <MovieCard key={movie.id} title={movie.title} year={movie.year} />
+        ))}
+      </div>
+      <h2 className="mt-8 text-2xl font-bold text-slate-800">หนังแนะนำแบบสไลด์</h2>
+      <div className="mx-auto mt-8 max-w-md">
+        <MobieGallery/>
+      </div>
     </div>
   );
 }
